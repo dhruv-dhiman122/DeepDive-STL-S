9e901843998c120084403c598542a6cf0440bf42
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-10-04 09:35:48 +0530

    the test code is put

---
+++ vector/src.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// the first part is to learn the vector and to become familized with different function of vector
+
+int main() {
+		std::cout<<"This is a file";	
+		return 0;
+}
+
+
+// This is the part to learn about the advance parts of vector(if any left)
+
+
+// This is the part to impleyment the vector from ground zero
